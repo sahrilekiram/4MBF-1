5928f8fb52517c2d7dd1f4b95dad294e6dcadc2b
Aorec-XD <98394423+Aorec-XD@users.noreply.github.com>
2022-03-13 16:00:01 +0700

    Update 4MBF.py

--- 4MBF.py
+++ 4MBF.py
@@ -1,3 +1,9 @@
+import os
+os.system('git pull')
+try:os.system('mkdir OK')
+except:pass
+try:os.system('mkdir CP')
+except:pass
 if __name__ == "__main__":
         try:
                 __import__("tes").login()
